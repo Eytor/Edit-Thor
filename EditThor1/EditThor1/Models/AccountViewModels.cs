7bbe9100becf4cf01a44cdd0d278c57ef7e9fa9a
Eyþór Atli Sigurðsson <Eyr Atli Sigursson>
2017-05-04 11:43:36 +0000

    removed username :´(

--- EditThor1/EditThor1/Models/AccountViewModels.cs
+++ EditThor1/EditThor1/Models/AccountViewModels.cs
@@ -66,10 +66,6 @@ namespace EditThor1.Models
     public class RegisterViewModel
     {
         [Required]
-        [Display(Name = "Username")]
-        public string UserName { get; set; }
-
-        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
