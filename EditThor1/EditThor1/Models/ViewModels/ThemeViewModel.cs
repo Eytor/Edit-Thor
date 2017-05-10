42fcbf564341e6306ade60d734d61b38dc86932c
Eyþór Atli Sigurðsson <Eyr Atli Sigursson>
2017-05-10 20:55:35 +0000

    appearance changes and more

--- EditThor1/EditThor1/Models/ViewModels/ThemeViewModel.cs
+++ EditThor1/EditThor1/Models/ViewModels/ThemeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +9,8 @@ namespace EditThor1.Models.ViewModels
 {
     public class ThemeViewModel
     {
+        [Required]
+        [Display(Name = "Theme")]
         public int themeID { get; set; }
 
         public string themeName { get; set; }
