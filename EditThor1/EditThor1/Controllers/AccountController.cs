a6df96a52a445c1c529fb336eb17e78dada7f25a
stebbih <stebbih@gmail.com>
2017-05-11 12:17:33 +0000

    set theme when user is created

--- EditThor1/EditThor1/Controllers/AccountController.cs
+++ EditThor1/EditThor1/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace EditThor1.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, themeId = 11 };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
