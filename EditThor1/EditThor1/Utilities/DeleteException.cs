9f04c00d8c53c504e274b816a0cf424276990578
svanhviti16 <svanhviti16@hr.is>
2017-05-11 18:52:32 +0000

    Fleiri error view

---
+++ EditThor1/EditThor1/Utilities/DeleteException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EditThor1.Utilities
+{
+    public class DeleteException : Exception
+    {
+        public DeleteException()
+        : base(String.Format("Project could not be deleted.")) { }
+
+        public DeleteException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ EditThor1/EditThor1/Utilities/NotRegisteredException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EditThor1.Utilities
+{
+    public class NotRegisteredException : Exception
+    {
+        public NotRegisteredException()
+        : base(String.Format("User not registered.")) { }
+
+        public NotRegisteredException(string message) : base(message)
+        {
+        }
+    }
+}
