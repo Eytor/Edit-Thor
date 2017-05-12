2d8a03229013215ea1618d6f94f953ae0c4842c8
eytor-atli <eythoras16@ru.is>
2017-05-12 12:32:41 +0000

    added more comments

--- EditThor1/EditThor1/Services/FileService.cs
+++ EditThor1/EditThor1/Services/FileService.cs
@@ -26,7 +26,7 @@ namespace EditThor1.Services
 
         private ApplicationDbContext _db = new ApplicationDbContext();
         // Function takes in data written in editor as byte array and updates file by file id.
-        public void SaveFile(byte[] arr, int fileId)
+        public void SaveFile(byte[] arr, int fileID)
         {
             File file = (from f in _db.Files
                         where f.ID == fileId
@@ -46,9 +46,9 @@ namespace EditThor1.Services
         }
 
         // Function deletes all files within a project by project id.
-        public void DeleteFiles(int? projectId)
+        public void DeleteFiles(int? projectID)
         {
-            List<File> files = _db.Files.Where(x => x.ProjectID == projectId).ToList();
+            List<File> files = _db.Files.Where(x => x.ProjectID == projectID).ToList();
             if(files != null)
             {
                 foreach(File file in files)
